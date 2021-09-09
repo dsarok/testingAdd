e5d6c9c29254037554c9dc81aae35f5005d32637
brahamjeetanand <divyashankaranand@gmail.com>
2021-09-09 15:02:18 +0530

    regex added to remove non word charecters

--- src/base/code/Stringcalculator.java
+++ src/base/code/Stringcalculator.java
@@ -7,29 +7,20 @@ public class Stringcalculator {
 				return 0;
 		}
 		else {
-			String []parts=numbers.split(",");
+			
 			String negative="";
 			int k=0;
-			for(int i=0;i<parts.length;i++) {
-				String[]parts2=parts[i].split("\n");
-				for(int j=0;j<parts2.length;j++) {
-					String[]parts3=parts2[j].split("/");
-					for(int l=0;l<parts3.length;l++) {
-						String[]parts4=parts3[l].split(";");
-						for(int m=0;m<parts4.length;m++) {
-							int h=Integer.parseInt(parts4[m]);
-							
-							if(h<0) {
-								
-								negative=negative+h+ " ";
-							}
-							else if(h<1000) {
-								k+=h;
-							}
-						}
-					}
+			String[] kh=(numbers.split("[\\W]+"));
+			for(int i=0;i<kh.length;i++) {
+				int h=Integer.parseInt(kh[i]);
+				if(h<1000 && h>0) {
+					k+=h;
+				}
+				else if(h<0) {
+					negative+=h+" ";
 				}
 			}
+			
 			if (negative.length()>0) {
 				throw new Exception("no negative numbers allowed"+negative);
 			}
--- src/base/code/testing.java
+++ src/base/code/testing.java
@@ -11,10 +11,17 @@ public class testing {
 		
 	}
 	@Test
-	public void singleinput() throws Exception {
+	public void negativenumber() throws Exception {
 		final int expected=143+99;
 		Stringcalculator addi=new Stringcalculator();
 		final int actual=addi.Add("143;99,-2,-4");
+		assertEquals(actual,expected);
+	}
+	@Test
+	public void simpleinput() throws Exception {
+		final int expected=143+99+12;
+		Stringcalculator addi=new Stringcalculator();
+		final int actual=addi.Add("143;99\n&&(***12");
 		assertEquals(actual,expected);
 	}
 }
